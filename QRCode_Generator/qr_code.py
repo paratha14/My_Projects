70a1f29d7abfff73585e64846094ea20f36e9bf0
Pratham Mohan <pratham.parker14@gmail.com>
2024-11-10 20:46:28 +0530

    Add files via upload

---
+++ QRCode_Generator/qr_code.py
@@ -0,0 +1,15 @@
+import qrcode
+#Creating QR Code
+import qrcode
+
+website_link = 'https://youtu.be/xvFZjo5PgG0?feature=shared'
+
+qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+qr.add_data(website_link)
+qr.make()
+
+img = qr.make_image(fill_color = 'black', back_color = 'white')
+img.save('QR.png')
+
+
+
